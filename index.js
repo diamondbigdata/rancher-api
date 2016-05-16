d64f4ccc1fbc44808dc9e2c7654ebadfd7509473
Nusrath Khan <nusrath501khan@gmail.com>
2016-05-16 22:31:52 +0200

    clean

--- index.js
+++ index.js
@@ -1,99 +1,10 @@
 module.exports = function rancher(host, port, user, pass){
 
-	var CSRF    = '';
-	var token   = '';
-	var headers = {};
-
-	auth();
-
-	function auth(){		
-
-		var http = require("http");
-
-		var options = {
-			"method": "GET",
-			"hostname": host,
-			"port": port,
-			"path": "/v1/token",
-			"headers": {
-				"x-api-no-challenge": "true",
-				"accept": "application/json",
-				"accept-encoding": "gzip, deflate, sdch",
-				"accept-language": "en-GB,en-US;q=0.8,en;q=0.6",
-				"cookie": "PL=rancher"
-			}
-		};
-
-		var req = http.request(options, function (res) {
-
-			var cookies = res.headers["set-cookie"];
-		    console.log(cookies);
-		    CSRF = cookis.cookie;
-		    login();
-
-		});
-
-		req.end();
-
-		function login(){
-
-			var http = require("http");
-
-			var options = {
-				"method": "POST",
-				"hostname": host,
-				"port": port,
-				"path": "/v1/token",
-				"headers": {
-					"content-type": "application/json",
-					"x-api-no-challenge": "true",
-					"accept": "application/json",
-					"x-api-csrf": CSRF,
-					"accept-encoding": "gzip, deflate",
-					"accept-language": "en-GB,en-US;q=0.8,en;q=0.6",
-					"cookie": "PL=rancher; CSRF=" + CSRF
-				}
-			};
-
-			var req = http.request(options, function (res) {
-
-				var chunks = "";
-
-				res.on("data", function (chunk) {
-				
-					chunks += chunk;
-				
-				});
-
-				res.on("end", function () {
-
-					console.log(JSON.parse(chunks));
-					var data = JSON.parse(chunks);
-					token = data.jwt;
-					headers = {
-						"content-type": "application/json",
-						"x-api-no-challenge": "true",
-						"accept": "application/json",
-						"x-api-csrf": CSRF,
-						"accept-encoding": "gzip, deflate",
-						"accept-language": "en-GB,en-US;q=0.8,en;q=0.6",
-						"cookie": "PL=rancher; CSRF=" + CSRF + "; token=" + token
-					}
-
-					console.log("CSRF: " + CSRF);
-					console.log("token: " + token);
-					console.log("headers: " + headers);
-
-				});
-
-			});
-
-			req.write(JSON.stringify({ code: user + ':' + pass, authProvider: 'localauthconfig' }));
-			req.end();
-
-		}
-
-	}
+	var headers = {
+		'Accept': 'application/json',
+		'Content-Type': 'application/json',
+		'Authorization': 'Basic ' + new Buffer(user + ':' + pass).toString('base64')
+	};
 
 	function call(method, url, body, callback)
 	{
@@ -141,9 +52,6 @@ module.exports = function rancher(host, port, user, pass){
 	}
 
 	return {
-		login: function login(){
-			auth();
-		},
 		create: function (data, callback){
 			call('POST', '/v1/projects/1a5/container', data, callback);
 		},
@@ -153,12 +61,14 @@ module.exports = function rancher(host, port, user, pass){
 		update: function (data, callback){
 			call('POST', '', data, callback);
 		},
-		delete: function (id, callback){
-			call('POST', '/v1/projects/1a5/containers/' + id + '/?action=stop', null, function(){
-				call('DELETE', '/v1/projects/1a5/containers/' + id, null, function(){
-					call('POST', '/v1/projects/1a5/containers/' + id + '/?action=stop', null, callback);
-				});
-			});
+		stop: function (id, callback){
+			call('POST', '/v1/projects/1a5/containers/' + id + '/?action=stop', null, callback);
+		},
+		remove: function (id, callback){
+			call('DELETE', '/v1/projects/1a5/containers/' + id, null, callback);
+		},
+		purg: function (id, callback){
+			call('POST', '/v1/projects/1a5/containers/' + id + '/?action=stop', null, callback);
 		}
 	}
 }
